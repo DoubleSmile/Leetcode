aa623bfdc6c7e4f962a091667928314550c8039d
吕彦峰 <425804471@qq.com>
2016-04-30 15:07:10 +0800

    帕斯卡三角形

---
+++ src/main/java/com/daily/April/PascalsTriangle.java
@@ -0,0 +1,37 @@
+package com.daily.April;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by luckyyflv on 16-4-30.
+ * Given numRows, generate the first numRows of Pascal's triangle.
+ * <p/>
+ * For example, given numRows = 5,
+ * Return
+ * <p/>
+ * [
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
+ * [1,4,6,4,1]
+ * ]
+ */
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> row, pre = null;
+        for (int i = 0; i < numRows; ++i) {
+            row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j)
+                if (j == 0 || j == i)
+                    row.add(1);
+                else
+                    row.add(pre.get(j - 1) + pre.get(j));
+            pre = row;
+            res.add(row);
+        }
+        return res;
+    }
+}
